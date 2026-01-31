4e82fb74d9d99adabb6e02d9f7e28e44e7c59ddf
geo_prepod_pc <romkagp@yandex.ru>
2026-01-31 12:05:58 +0300

    Initial commit

---
+++ steam_path.py
@@ -0,0 +1,21 @@
+import winreg
+
+
+def get_windows_steam_path():
+    try:
+        main_path = r"Software\Valve\Steam"
+        hkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, main_path)
+        path, code = winreg.QueryValueEx(hkey, "SteamPath")
+        hkey.Close()
+        return path
+    except FileNotFoundError:
+        print("Steam not found.")
+        return None
+    except Exception as e:
+        print(f"An error while reading: {e}")
+        return None
+
+
+steam_location = get_windows_steam_path()
+if steam_location:
+    print(f"Steam установлен в: {steam_location}")
